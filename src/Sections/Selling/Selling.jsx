0c627ce6c44aee4df59187c21ca4aabdba6a2295
Asad Khan <mrasad10khan@gmail.com>
2025-03-24 16:52:36 +0500

    Create Selling Section

--- src/Sections/Selling/Selling.jsx
+++ src/Sections/Selling/Selling.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import rome1 from "../../assets/rome1.jpg";
 import rome2 from "../../assets/rome2.jpg";
 import rome3 from "../../assets/rome3.jpg";
+import sellingData from "../../data/SellingData";
 
 const Selling = () => {
   return (
-    <section className="py-[50px]">
+    <section className="py-[50px] lg:pb-[100px]">
       <div className="max-w-[80rem] h-auto px-12 mx-auto lg:px-16">
         <div className="text-center">
           <div className="text-[#5E6282] text-[14px] font-semibold poppins-text mb-2.5 lg:text-lg  ">
@@ -17,28 +18,35 @@ const Selling = () => {
         </div>
 
         <div className="pt-[60px]">
-          <div className="h-[457px] flex gap-[35px] flex-wrap ">
-            <div className="w-full ">
-              <div className="selling-card-shadow rounded-3xl overflow-hidden  ">
-                <div className="h-[327px]">
-                  <img
-                    src={rome1}
-                    alt="rome-image"
-                    className="h-full w-full object-cover"
-                  />
-                </div>
+          <div className="flex gap-[35px] flex-wrap lg:mx-[-35px] lg:gap-0 justify-center  ">
+            {sellingData.map((info, index) => (
+              <div className="w-full lg:w-[25%] lg:px-4 " key={index}>
+                <div className="selling-card-shadow rounded-3xl overflow-hidden hover:translate-y-[-10%] transition-all duration-[1s]">
+                  <div className="h-[327px]">
+                    <img
+                      src={info.image}
+                      alt="rome-image"
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
 
-                <div>
-                  <div className="pl-[20px] pt-[27px] pb-[20px]   poppins-text">
-                    <div className="flex items-center justify-between  pb-[22px] pr-[26px] text-[16px]">
-                      <div>Rome,Italy</div>
-                      <div>$5,42K</div>
+                  <div className="px-[20px] pt-[27px] pb-[20px]   poppins-text font-normal text-[#5E6282]">
+                    <div className="flex items-center justify-between  mb-[19px]  text-[16px]">
+                      <div>{info.place}</div>
+                      <div>${info.price}K</div>
                     </div>
-                    <div className="text-[14px]">10 Days Trip</div>
+                    <div className="flex text-[14px] items-center gap-[14px] ">
+                      {" "}
+                      <ion-icon
+                        name="navigate"
+                        className="text-black"
+                      ></ion-icon>{" "}
+                      {info.time} Days Trip
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/data/SellingData.js
@@ -0,0 +1,11 @@
+import image1 from "../assets/rome1.jpg";
+import image2 from "../assets/rome2.jpg";
+import image3 from "../assets/rome3.jpg";
+
+const sellingData = [
+  { image: image1, place: "Rome,Italy", price: "5,42", time: "10" },
+  { image: image2, place: "London,UK", price: "4.2", time: "12" },
+  { image: image3, place: "Full Europe", price: "15", time: "28" },
+];
+
+export default sellingData;
