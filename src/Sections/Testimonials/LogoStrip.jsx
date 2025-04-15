c8e99f2526abcda50ecd2b9d60ebb48c15f72354
Asad Khan <mrasad10khan@gmail.com>
2025-04-15 18:03:48 +0500

    Create Testimonails Section

--- src/Sections/Testimonials/LogoStrip.jsx
+++ src/Sections/Testimonials/LogoStrip.jsx
@@ -6,33 +6,39 @@ import company4 from "../../assets/company4.png";
 import company5 from "../../assets/company5.png";
 const LogoStrip = () => {
   return (
-    <div className="w-full overflow-auto mt-15 flex justify-start">
+    <div className="w-full overflow-auto mt-32 flex justify-start ">
       <div className="flex gap-4 w-max items-center lg:gap-18 ">
-        <img
-          src={company1}
-          alt="Company logo"
-          className="flex-none filter grayscale hover:grayscale-0 transition duration-300"
-        />
-        <img
-          src={company2}
-          alt="Company logo"
-          className="flex-none filter grayscale hover:grayscale-0 transition duration-300"
-        />
-        <img
-          src={company3}
-          alt="Company logo"
-          className="flex-none filter grayscale hover:grayscale-0 transition duration-300"
-        />
-        <img
-          src={company4}
-          alt="Company logo"
-          className="flex-none filter grayscale hover:grayscale-0 transition duration-300"
-        />
-        <img
-          src={company5}
-          alt="Company logo"
-          className="flex-none filter grayscale hover:grayscale-0 transition duration-300"
-        />
+        <div className="testimonial-container  hover:shadow-lg">
+          <img
+            src={company1}
+            alt="Company logo"
+            className=" testimonial-logo "
+          />
+        </div>
+        <div className="testimonial-container  hover:shadow-">
+          <img
+            src={company2}
+            alt="Company logo"
+            className=" testimonial-logo"
+          />
+        </div>
+        <div className="testimonial-container hover:shadow-lg">
+          <img
+            src={company3}
+            alt="Company logo"
+            className=" testimonial-logo"
+          />
+        </div>
+        <div className="testimonial-container  hover:shadow-lg">
+          <img
+            src={company4}
+            alt="Company logo"
+            className=" testimonial-logo"
+          />
+        </div>
+        <div className="testimonial-container  hover:shadow-lg">
+          <img src={company5} alt="Company logo" className="testimonial-logo" />{" "}
+        </div>
       </div>
     </div>
   );
